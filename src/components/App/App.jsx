fdf199edb1919a8ec5184e6dbe7c43b0fdeae42a
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-23 17:34:13 -0500

    Input some pictures into the imgages folder and put the images in the divs.  I started on some CSS to help with resizing and placement.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -8,7 +8,17 @@ function App() {
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
         <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
+        {/* container 1 for first row of images */}
+        <div class='container'>
+         <div class='img1'><img src="images/goat_small.jpg"/></div> 
+         <div class='img2'><img src="images/EGA_Small.jpeg"/></div>
+          <div class='img3'><img src="images/breading_Small.jpeg"/></div>
+          </div>  
+          {/* container 2 for second row of images */}
+        <div class='conatainer2'>
+          <div class='img4'> <img src="images/mem_Small.jpeg"/> </div>
+          <div class='img5'> <img src="images/drawing_small.jpeg"/> </div>
+      </div>
       </div>
     );
 }
